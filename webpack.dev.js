2594b88878c05ab9d635b8226e84383e11071819
yasmine.abdallah <yasmine.abdallah@almtar.com>
2021-04-23 19:59:10 +0200

    add workbloxplugin in dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -18,21 +18,21 @@ module.exports = {
         rules: [
                 {
                     test: '/\.js$/',
-            exclude: /node_modules/,
-            loader: "babel-loader"
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
 
                 },
                 {
                     test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-            }
+                    use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                    ],
+                }
             
         ]
     },
@@ -46,6 +46,8 @@ module.exports = {
             verbose: true,
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        }),        
+        }),  
+        new WorkboxPlugin.GenerateSW(),
+      
     ]
 }
